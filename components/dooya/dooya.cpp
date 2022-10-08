91905b1bbc254594cfca594c9f36dcfc23f01d0b
pruwait <pruwait@gmail.com>
2022-10-08 15:50:47 +0300

    Update dooya.cpp

--- components/dooya/dooya.cpp
+++ components/dooya/dooya.cpp
@@ -98,7 +98,7 @@ void Dooya::on_uart_multi_byte(uint8_t byte) { // вызывается при п
     
       std::vector<uint8_t> frame(this->rx_buffer_.begin(), this->rx_buffer_.end());
       uint16_t crc = crc16(&frame[0], frame.size());      // получили crc
-      if (((crc & 0xFF) == this->rx_buffer_.end()[-1]) && ((crc >> 8) == this->rx_buffer_.end()[-2])) {  // если пришло всё сообщение
+      if (((crc & 0xFF) == this->rx_buffer_.end()[-2]) && ((crc >> 8) == this->rx_buffer_.end()[-1])) {  // если пришло всё сообщение
         if (this->rx_buffer_[3] == CONTROL)
           this->process_response_();
         else
