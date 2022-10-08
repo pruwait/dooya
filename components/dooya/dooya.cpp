ec96a3320f5fe822e5d116a832e9520c2890e288
pruwait <pruwait@gmail.com>
2022-10-08 15:42:14 +0300

    Update dooya.cpp

--- components/dooya/dooya.cpp
+++ components/dooya/dooya.cpp
@@ -64,27 +64,32 @@ void Dooya::on_uart_multi_byte(uint8_t byte) { // вызывается при п
   if (at == 0) {
       if (byte == START_CODE)
         this->rx_buffer_.push_back(byte);   // получили заголовок = байт0
+       return; 
   }
   if (at == 1) {
       if (byte == this->address_[0])
         this->rx_buffer_.push_back(byte);    // получили адрес0
       else
         this->rx_buffer_.clear();
+      return;
   }
   if (at == 2) {
       if (byte == this->address_[1])
         this->rx_buffer_.push_back(byte);    // получили адрес1
       else
         this->rx_buffer_.clear();
+      return;
   }      
   if (at == 3) {
       if (byte == CONTROL || byte == READ)  
         this->rx_buffer_.push_back(byte);    // получили команду управлния или чтения
       else
         this->rx_buffer_.clear();
+      return;
   }
   if (at < 6) {                    // получили данные и возможно, начало crc 
       this->rx_buffer_.push_back(byte);
+      return;
   }      
   if (at >= 6)  {                    // возможно, получили весь пакет
       this->rx_buffer_.push_back(byte);
@@ -99,7 +104,7 @@ void Dooya::on_uart_multi_byte(uint8_t byte) { // вызывается при п
         else
           this->process_status_();
       this->rx_buffer_.clear();
-      
+      return;
     } //  if (at >= 6) 
 
       
